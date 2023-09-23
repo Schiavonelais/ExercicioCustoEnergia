36162d6606b2f7c31c68a5f9769b59950e4c691a
Lais Schiavone Sobral <a202304133441@alunos.estacio.br>
2023-09-23 13:11:53 -0300

    Completo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double consDia, consApar, custEner, custUso;
+Console.Write("Consumo do aparelho (Em kWh/mês):..."); 
+consApar = Convert.ToDouble(Console.ReadLine());
+    
+Console.Write("Horas de uso diario:...");
+consDia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Custo de energia (Em R$/kWh):...");
+custEner = Convert.ToDouble(Console.ReadLine());
+
+custUso = consDia * consApar * custEner; 
+Console.WriteLine($"Custo estimado: R$ {custUso:N2}");
